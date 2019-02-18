7f618a93ef174f0297d6f6b47e69d35146e9bb22
Himanshu Shekhar Jha <himanshu@beyondsquare.com>
2019-02-18 17:30:35 +0530

    Added more resolution while scanning qr-code

---
+++ dev/qrscan/myQrScanner.js
@@ -0,0 +1,10 @@
+import QrScanner from 'qr-scanner.min.js';
+QrScanner.WORKER_PATH = 'qr-scanner-worker.min.js';
+
+const qrScanner = new QrScanner(videoElem, result => console.log('decoded qr code:', result));
+QrScanner.scanImage(image)
+    .then(result => console.log(result))
+    .catch(error => console.log(error || 'No QR code found.'));
+qrScanner.setGrayscaleWeights(red, green, blue, useIntegerApproximation = true);
+qrScanner.destroy();
+qrScanner = null;
