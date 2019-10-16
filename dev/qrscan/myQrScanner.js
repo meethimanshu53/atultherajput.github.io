b9ab6cd81e183b6b265186c03d8a883027e9e48a
Himanshu Shekhar Jha <himanshu@beyondsquare.com>
2019-10-17 00:41:53 +0530

    Changed console.log message

--- dev/qrscan/myQrScanner.js
+++ dev/qrscan/myQrScanner.js
@@ -2,7 +2,7 @@ import QrScanner from './qr-scanner.min.js';
 QrScanner.WORKER_PATH = './qr-scanner-worker.min.js';
 
 const videoElem = document.getElementById('preview');
-const qrScanner = new QrScanner(videoElem, result => { console.log('decoded qr code:', result); alert(result); });
+const qrScanner = new QrScanner(videoElem, result => { console.log('decoded qr code is here:', result); alert(result); });
 
 qrScanner.start();
 //qrScanner.setGrayscaleWeights(red, green, blue, useIntegerApproximation = true);
